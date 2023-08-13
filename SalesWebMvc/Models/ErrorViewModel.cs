e6f42a1ff2da19a28e46a9b00af1273e06a9679c
Marcus1g2 <marcusdm157@outlook.com>
2023-08-13 12:28:00 -0300

    excluindo alguns arquivos departamento

--- SalesWebMvc/Models/ErrorViewModel.cs
+++ SalesWebMvc/Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace SalesWebMvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace SalesWebMvc.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
