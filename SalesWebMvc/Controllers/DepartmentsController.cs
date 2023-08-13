592259dfa61c7428d9998a83861453a4b38c6c2c
Marcus1g2 <marcusdm157@outlook.com>
2023-08-12 21:51:00 -0300

    criação de lista, e ligando com o front

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> nList = new List<Department>();
+
+            nList.Add(new Department { Id = 1, Name = "Eletronicos", });
+            nList.Add(new Department { Id = 2, Name = "Moda" });
+            nList.Add(new Department { Id = 3, Name = "Cozinha" });
+
+
+
+            return View(nList);
+        }
+    }
+}
