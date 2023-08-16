37a55aec597f45a0fe569bdccd392a1fa04f33c6
Marcus1g2 <marcusdm157@outlook.com>
2023-08-16 14:44:31 -0300

    migração de dados entre VS e o Banco de dados

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,8 +1,17 @@
-﻿namespace SalesWebMvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesWebMvc.Models
 {
+ 
     public class Department
     {
+        public Department()
+        {
+            // Inicialize a propriedade Name aqui
+            Name = ""; // Ou qualquer outro valor padrão que você preferir
+        }
         public int Id { get; set; }
+
         public string Name { get; set; }
     }
 }
--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿
+using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<SalesWebMvcContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMvcContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMvcContext' not found.")));
+
+
+// Add services to the container.
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+string mySqlConnection =
+              builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContextPool<SalesWebMvcContext>(options =>
+                options.UseMySql(mySqlConnection,
+                      ServerVersion.AutoDetect(mySqlConnection)));
 
 var app = builder.Build();
 
